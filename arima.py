104791fd58ead72274ad63b0e3032159dff29f20
Yuchong (Charles) Chen <58545024+icharleschen@users.noreply.github.com>
2022-04-25 15:54:25 -0700

    Update Model Parameters

--- arima.py
+++ arima.py
@@ -92,7 +92,7 @@ def arimaPredict(df, structure):
     y = train['Free Spaces']
 
     # Set up ARIMA model
-    arima_model = ARIMA(y, order=(2, 0, 1))
+    arima_model = ARIMA(y, order=(4, 1, 1))
     arima_model = arima_model.fit()
 
     # Train ARIMA model
